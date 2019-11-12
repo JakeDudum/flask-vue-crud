3e26da1f7f2d98b6b02279ecff338fb9d15efbf2
Jake Dudum <jakedudum@gmail.com>
2019-11-12 11:12:09 -0800

    Updated books component with bootstrap elements and styling as well as added json data for the server to return

--- server/app.py
+++ server/app.py
@@ -1,6 +1,24 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
 # configuration
 DEBUG = True
 
@@ -12,6 +30,13 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 # sanity check route
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
+    
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
